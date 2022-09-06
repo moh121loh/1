c9edc80e1eb21a89e088627f401a5cc218846839
moh121loh <112933463+moh121loh@users.noreply.github.com>
2022-09-06 10:32:33 +0100

    Update start.bat

--- .github/workflows/start.bat
+++ .github/workflows/start.bat
@@ -12,6 +12,7 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+echo  Successful installation !, If RDP is dead, please rebuild again! 
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
