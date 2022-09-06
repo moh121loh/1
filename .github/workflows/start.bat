2697eddcb17389c46bc6d622985c0969b67786db
moh121loh <112933463+moh121loh@users.noreply.github.com>
2022-09-06 10:21:50 +0100

    Update start.bat

--- .github/workflows/start.bat
+++ .github/workflows/start.bat
@@ -12,8 +12,7 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-"%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AdQt8qhTofV2Z4j96_CDoChh08TyouR7Uds2qcH0pWKhACZml-6-j20vZ0O6q8iiLWX7gQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=%COMPUTERNAME%
-echo  Successful installation !, If RDP is dead, please rebuild again! 
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AdQt8qhTofV2Z4j96_CDoChh08TyouR7Uds2qcH0pWKhACZml-6-j20vZ0O6q8iiLWX7gQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAMEecho  Successful installation !, If RDP is dead, please rebuild again! 
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
